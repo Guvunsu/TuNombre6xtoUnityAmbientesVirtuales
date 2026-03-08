277688a0a861466b2954f10f9327d6dc0b5aed3b
Guvunsu <gavryk_fray_rojas@hotmail.com>
2026-03-08 01:28:31 -0600

    PARCIAL COMPLETADO

--- TuNombre6xtoAmbientesVirtuales/Assets/script/Parcial1 StarWars/GameStarWarsManager.cs
+++ TuNombre6xtoAmbientesVirtuales/Assets/script/Parcial1 StarWars/GameStarWarsManager.cs
@@ -6,7 +6,7 @@ public class GameStarWarsManager : MonoBehaviour
     public GameObject[] pisosColapsables;
     public GameObject greenSable;
     public GameObject blueSable;
-    public AudioSource alarma;
+    //public AudioSource alarma;
     float tiempoEntreColapso = 9.10f;
     public void ActivarEventoFinal()
     {
@@ -15,15 +15,18 @@ public class GameStarWarsManager : MonoBehaviour
 
         greenSable.SetActive(true);
         blueSable.SetActive(true);
-        alarma.Play();
+        //alarma.Play();
         StartCoroutine(ColapsarPisos());
-    }
+    } 
     IEnumerator ColapsarPisos()
     {
         foreach (GameObject piso in pisosColapsables)
         {
-            yield return new WaitForSeconds(tiempoEntreColapso); 
-            piso.SetActive(false);
+            Debug.Log("Esperando para colapsar piso");
+            yield return new WaitForSeconds(tiempoEntreColapso);
+            Debug.Log("Colapsando: " + piso.name);
+            Rigidbody rb = piso.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
         }
     }
 }
