c9119e2a756c3b337a8399df250eac500aa17ec5
Guvunsu <gavryk_fray_rojas@hotmail.com>
2026-05-04 21:05:09 -0600

    listo

--- TuNombre6xtoAmbientesVirtuales/Assets/Scenes/Parcial3/tarea1/scrips1er/XRButtonReadyFood.cs
+++ TuNombre6xtoAmbientesVirtuales/Assets/Scenes/Parcial3/tarea1/scrips1er/XRButtonReadyFood.cs
@@ -2,15 +2,94 @@ using UnityEngine;
 
 public class XRButtonReadyFood : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum FoodState
     {
-        
+        Tortilla,
+        Frijoles,
+        Carne,
+        Vegetales,
+        Completa
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Estado Actual")]
+    public FoodState currentState;
+
+    [Header("Modelos o Etapas")]
+    public GameObject[] foodStages;
+
+    [Header("FX Finales")]
+    public ParticleSystem fireParticles;
+    public ParticleSystem vaporParticles;
+
+    [Header("Canvas Final")]
+    public GameObject finalCanvas;
+
+    [Header("Delivery")]
+    public XRDeliveryFood deliverySystem;
+
+    private int currentIndex = 0;
+
+    void Start()
     {
-        
+        UpdateFoodVisual();
+
+        if (finalCanvas != null)
+            finalCanvas.SetActive(false);
+
+        if (fireParticles != null)
+            fireParticles.gameObject.SetActive(false);
+
+        if (vaporParticles != null)
+            vaporParticles.gameObject.SetActive(false);
+    }
+
+    //BOTÓN XR
+    public void NextFoodState()
+    {
+        currentIndex++;
+
+        if (currentIndex >= foodStages.Length)
+        {
+            currentIndex = foodStages.Length - 1;
+
+            currentState = FoodState.Completa;
+
+            ActivateFinalEffects();
+
+            if (deliverySystem != null)
+                deliverySystem.canDeliver = true;
+
+            return;
+        }
+
+        currentState = (FoodState)currentIndex;
+
+        UpdateFoodVisual();
+    }
+
+    void UpdateFoodVisual()
+    {
+        for (int i = 0; i < foodStages.Length; i++)
+        {
+            foodStages[i].SetActive(i == currentIndex);
+        }
+    }
+
+    void ActivateFinalEffects()
+    {
+        if (finalCanvas != null)
+            finalCanvas.SetActive(true);
+
+        if (fireParticles != null)
+        {
+            fireParticles.gameObject.SetActive(true);
+            fireParticles.Play();
+        }
+
+        if (vaporParticles != null)
+        {
+            vaporParticles.gameObject.SetActive(true);
+            vaporParticles.Play();
+        }
     }
 }
--- TuNombre6xtoAmbientesVirtuales/Assets/Scenes/Parcial3/tarea1/scrips1er/XRDeliveryFood.cs
+++ TuNombre6xtoAmbientesVirtuales/Assets/Scenes/Parcial3/tarea1/scrips1er/XRDeliveryFood.cs
@@ -2,15 +2,72 @@ using UnityEngine;
 
 public class XRDeliveryFood : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Tlayuda")]
+    public GameObject foodObject;
+
+    [Header("Punto de Entrega")]
+    public Transform deliveryPoint;
+
+    [Header("Distancia para Entregar")]
+    public float deliveryDistance = 0.25f;
+
+    [Header("Canvas UI")]
+    public GameObject deliveryCanvas;
+
+    [Header("FX")]
+    public ParticleSystem deliveryParticles;
+
+    [HideInInspector]
+    public bool canDeliver = false;
+
+    private bool delivered = false;
+
     void Start()
     {
-        
+        if (deliveryCanvas != null)
+            deliveryCanvas.SetActive(false);
+
+        if (deliveryParticles != null)
+            deliveryParticles.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!canDeliver || delivered)
+            return;
+
+        if (foodObject == null || deliveryPoint == null)
+            return;
+
+        float distance = Vector3.Distance(
+            foodObject.transform.position,
+            deliveryPoint.position
+        );
+
+        if (distance <= deliveryDistance)
+        {
+            DeliverFood();
+        }
+    }
+
+    void DeliverFood()
+    {
+        delivered = true;
+
+        Debug.Log("Tlayuda Entregada");
+
+        if (deliveryCanvas != null)
+            deliveryCanvas.SetActive(true);
+
+        if (deliveryParticles != null)
+        {
+            deliveryParticles.gameObject.SetActive(true);
+            deliveryParticles.Play();
+        }
+
+        if (foodObject != null)
+        {
+            foodObject.SetActive(false);
+        }
     }
 }
